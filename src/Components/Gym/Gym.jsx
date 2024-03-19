b40521548798f0d94e1141814f2cc47f0e1002e9
DX-Sumon <dxsumon14567@gmail.com>
2024-03-19 23:35:41 +0600

    card bg Change and the hamburger update

--- src/Components/Gym/Gym.jsx
+++ src/Components/Gym/Gym.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 const Gym = ({gym}) => {
     const {name,location,facilities,price} = gym;
     return (
-        <div className="bg-rose-500 text-white p-5 flex flex-col justify-center rounded-xl">
+        <div className="bg-gray-500 text-white p-5 flex flex-col justify-center rounded-xl">
             <h1 className="text-2xl font-bold">Name: {name}</h1>
             <h2>Location: {location}</h2>
             <p>Facilities: {facilities.map((item,id)=> <li key={id}>{item}</li>)}</p>
--- src/Components/Link/Link.jsx
+++ src/Components/Link/Link.jsx
@@ -4,7 +4,7 @@ const Link = ({route}) => {
     const {name,path} = route;
     return (
         <div>
-            <li className='hover:bg-rose-500 px-6'><a href={path}>{name}</a></li>
+            <li className='hover:bg-gray-400 hover:text-white px-6'><a href={path}>{name}</a></li>
         </div>
     );
 };
